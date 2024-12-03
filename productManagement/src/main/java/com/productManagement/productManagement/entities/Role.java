fe0903db2ac60e10900347b363a41fb10f2b693e
TERMOUSSI-LAMIAA <lamiaa3105@gmail.com>
2024-12-03 10:33:44 +0100

    PROD-1 User & Role Entities

---
+++ productManagement/src/main/java/com/productManagement/productManagement/entities/Role.java
@@ -0,0 +1,21 @@
+package com.productManagement.productManagement.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+}
---
+++ productManagement/src/main/java/com/productManagement/productManagement/entities/User.java
@@ -0,0 +1,35 @@
+package com.productManagement.productManagement.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+
+    private Boolean active;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Collection<Role> roles;
+
+}
